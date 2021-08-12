d23d13820815f7278debb3b12f14b6fe02935b6f
Tektronica <ryalho@gmail.com>
2021-08-11 22:13:05 -0700

    fixed ligthbox and merged image-with-fallback image component w/ ExampleWithLightbox

--- components/ExampleWithLightbox.js
+++ components/ExampleWithLightbox.js
@@ -2,9 +2,9 @@
 import React, { useState, useCallback } from 'react';
 import Gallery from 'react-photo-gallery';
 import Carousel, { Modal, ModalGateway } from 'react-images';
-import ImageWithFallback from '../lib/image-with-fallback';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import Image from "next/image"
+import Image from 'next/image';
+// import ImageWithFallback from '../lib/image-with-fallback';
+
 
 const shimmer = (w, h) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -20,11 +20,15 @@ const shimmer = (w, h) => `
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
 </svg>`
 
+
 const toBase64 = (str) =>
   typeof window === 'undefined'
     ? Buffer.from(str).toString('base64')
     : window.btoa(str)
 
+
+// Response (dynamic) columns
+// https://github.com/neptunian/react-photo-gallery/blob/0bb8e4c4a027c021f8a5a06de71e89026596fd95/examples/src/ExampleDynamicColumns.js
 function columns(containerWidth) {
   let columns = 1;
   if (containerWidth >= 768) columns = 2;
@@ -33,7 +37,58 @@ function columns(containerWidth) {
   return columns;
 }
 
+
+// Custom Image rendering object
+// https://github.com/neptunian/react-photo-gallery/issues/171#issuecomment-631057102
+// https://github.com/neptunian/react-photo-gallery/issues/178#issuecomment-845826413
+function ImageWithFallback(props) {
+
+  const { fallbackSrc, ...photoObj } = props;
+  const { index, photo, margin, direction, top, left, onClick, ...rest } = { ...photoObj };
+  const [imgSrc, setImgSrc] = useState(photo.src);
+
+  const cont = {
+    backgroundColor: '#eee',
+    cursor: 'pointer',
+    overflow: 'hidden',
+    position: 'relative',
+  };
+
+  if (direction === 'column') {
+    cont.position = 'absolute';
+    cont.left = left;
+    cont.top = top;
+  }
+
+  return (
+    <div className={"image-item"}
+
+      role={"button"}
+      tabIndex={0}
+      style={{
+        width: photo.width,
+        height: photo.height,
+        margin,
+
+        // the important staff
+        position: "absolute",
+        left,
+        top,
+      }}
+      onClick={onClick}
+    >
+      <Image
+        {...photo}
+        onError={() => { setImgSrc(fallbackSrc) }}
+      />
+    </div>
+  );
+};
+
+
+// Responsive masonry gallery with lightbox 
 function ExampleWithLightbox({ photos }) {
+
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
@@ -47,16 +102,22 @@ function ExampleWithLightbox({ photos }) {
     setViewerIsOpen(false);
   };
 
+  // https://github.com/neptunian/react-photo-gallery/issues/200#issuecomment-887995313
+  const customStyles = {
+    view: () => ({
+      // none of react-images styles are passed to <View />
+      position: 'relative',
+      '& > img': {
+        position: 'relative',
+        margin: '0 auto'
+      },
+    })
+  };
 
   // optional; use a different image component than the default provided to display photo
   // https://github.com/neptunian/react-photo-gallery/issues/178#issuecomment-845826413
-  const imageRenderer = useCallback(({ margin, index, photo, left, top, key, onClick }) => (
+  const imageRenderer = useCallback(({ margin, index, photo, left, top, key }) => (
     <div className="image-item" style={{ margin: '5px' }}>
-
-      {/* <LazyLoadImage
-        className="image-hover" 
-        {...photo} style={{ top: top, left: left }}
-      /> */}
 
       <ImageWithFallback
         // src={photo.src}
@@ -67,8 +128,7 @@ function ExampleWithLightbox({ photos }) {
 
         left={left}
         top={top}
-
-        onClick={openLightbox}
+        onClick={(e) => { openLightbox(e, { photo, index }) }}
 
         placeholder="blur"
         blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
@@ -76,18 +136,6 @@ function ExampleWithLightbox({ photos }) {
         // https://source.unsplash.com/
         fallbackSrc={`https://source.unsplash.com/${Math.round(photo.width)}x${Math.round(photo.height)}/?${photo.album}`}
       />
-
-      {/* <LazyLoadImage
-                alt={photo.title}
-                effect="blur"
-                className="image-hover"
-                width={300}
-                height={photo.height * 300 / photo.width}
-                placeholderSrc={`https://source.unsplash.com/${300}x${Math.round(photo.height * 300 / photo.width)}/?${photo.album}`}
-                src={`https://source.unsplash.com/${300}x${Math.round(photo.height * 300 / photo.width)}/?${photo.album}`}
-                style={{top: top, left: left}}
-            />
-        */}
 
     </div>
   ));
@@ -108,6 +156,7 @@ function ExampleWithLightbox({ photos }) {
           <Modal onClose={closeLightbox}>
             <Carousel
               currentIndex={currentImage}
+              styles={customStyles}
               views={photos.map(x => ({
                 ...x,
                 srcset: x.srcSet,
